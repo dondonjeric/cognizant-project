68fb0f7d7ffeb0a82929db81fb354d8e8906377c
Dean Angelo Vonn Enalpe <vonnenalpe@gmail.com>
2022-11-23 13:15:08 +0800

    edited display name and comments, added controllertest

--- src/test/java/com/academy/project/CommunityAdminAndManagerServiceImplTest.java
+++ src/test/java/com/academy/project/CommunityAdminAndManagerServiceImplTest.java
@@ -31,12 +31,12 @@ public class CommunityAdminAndManagerServiceImplTest {
     @InjectMocks
     CommunityAdminAndManagerServiceImpl communityAdminAndManagerService;
 
-    CommunityAdminAndManager communityAdminAndManager1 = new CommunityAdminAndManager(1L,"Dondon Vic Ali", "veripro@gmail.com", "veripro", "admin1", "admin", true);
-    CommunityAdminAndManager communityAdminAndManager2 = new CommunityAdminAndManager(1L,"Marvin Jerome Shawn", "veripro@gmail.com", "veripro", "admin1", "admin", true);
-    CommunityAdminAndManager communityAdminAndManager3 = new CommunityAdminAndManager(1L,"Baki Kel Lyoys", "veripro@gmail.com", "veripro", "admin1", "admin", true);
-    CommunityAdminAndManager communityAdminAndManager4 = new CommunityAdminAndManager(1L,"Dean Angelo Vonn", "veripro@gmail.com", "veripro", "admin1", "admin", true);
+    private CommunityAdminAndManager communityAdminAndManager1 = new CommunityAdminAndManager(1L,"Dondon Vic Ali", "veripro@gmail.com", "veripro", "admin1", "admin", true);
+    private CommunityAdminAndManager communityAdminAndManager2 = new CommunityAdminAndManager(1L,"Marvin Jerome Shawn", "veripro@gmail.com", "veripro", "admin1", "admin", true);
+    private CommunityAdminAndManager communityAdminAndManager3 = new CommunityAdminAndManager(1L,"Baki Kel Lyoys", "veripro@gmail.com", "veripro", "admin1", "admin", true);
+    private CommunityAdminAndManager communityAdminAndManager4 = new CommunityAdminAndManager(1L,"Dean Angelo Vonn", "veripro@gmail.com", "veripro", "admin1", "admin", true);
 
-    List<CommunityAdminAndManager> allCommunityAdminAndManager;
+    private List<CommunityAdminAndManager> allCommunityAdminAndManager;
     @BeforeEach
     public void setup(){
         allCommunityAdminAndManager = List.of(communityAdminAndManager1, communityAdminAndManager2, communityAdminAndManager3, communityAdminAndManager4);
@@ -61,21 +61,18 @@ public class CommunityAdminAndManagerServiceImplTest {
     }
     @Test
     @DisplayName("" +
-            "Given Payroll with the setup above " +
+            "Given CommunityManager with the setup above " +
             "When updateComManager(CommunityAdminAndManager, Long) is executed " +
             "Then result should return updated communityAdminAndManager3")
     public void updateComManager() throws RecordNotFoundException, InvalidStringFormatException {
-        //ARRANGE
+        //arrange
         CommunityAdminAndManager update = communityAdminAndManager3;
         update.setName("updateName");
-
-        // Mocking payroll methods for service method dependency
         when(communityManagerRepository.findById(anyLong())).thenReturn(Optional.ofNullable(communityAdminAndManager3));
         when(communityManagerRepository.save(any(CommunityAdminAndManager.class))).thenReturn(update);
-        //ACT
+        //act
         CommunityAdminAndManager result = communityAdminAndManagerService.updateCommunityManagerAndAdmin(update, 3L);
-        //ASSERT
-        // verify if payrollRepository.save() is use
+        //assert
         verify(communityManagerRepository).save(any(CommunityAdminAndManager.class));
         assertEquals(communityAdminAndManager3, result);
     }
